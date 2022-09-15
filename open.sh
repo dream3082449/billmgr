f8b729f60d523d7c92e958f4d86b28ac12b7e59f
Иван Котырло <shonishe@gmail.com>
2022-09-15 23:23:44 +0700

    test 111

--- open.sh
+++ open.sh
@@ -6,7 +6,7 @@ do
 done
 resp=`python3 /opt/billmgr/wrapper.py commandfile=open $s_args`
 IFS=' '
-read -a strarr <<< $resp
-r=`python3 /opt/billmgr/callback.py --request_id=${strarr[0]}`
-
+#read -a strarr <<< $resp
+#r=`python3 /opt/billmgr/callback.py --request_id=${strarr[0]}`
+r=1243
 echo "OK $r"
