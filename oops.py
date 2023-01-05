c0c47a936baa720a6093d82d624e48e38cf7cc70
Иван Котырло <shonishe@gmail.com>
2023-01-05 19:17:29 +0700

    fix 3

--- oops.py
+++ oops.py
@@ -106,7 +106,6 @@ class oops_helper(object):
 
     def create_instance(self, project, params):
         p = {
-            'project': project,
             "admin_password": params.get("password"),
             "name" : params.get("instance_name"),
             "instance_name" : params.get("meta_name"),
