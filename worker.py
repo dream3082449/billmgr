38ddf516ed85d1c3a7b2933eb21afd269c4fb79e
Shonishe <shonishe@gmail.com>
2023-02-16 14:13:08 +0700

    fix p2 err

--- worker.py
+++ worker.py
@@ -324,6 +324,6 @@ if __name__ == "__main__":
                         sys.exit(2)
                 sys.exit(0)
         else:
-                print "usage: %s start|stop|restart" % sys.argv[0]
+                print("usage: %s start|stop|restart" % sys.argv[0])
                 sys.exit(2)
 
